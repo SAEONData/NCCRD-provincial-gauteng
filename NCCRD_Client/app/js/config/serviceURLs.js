3bc6aa232981c09c8ce0c1e0c12df9c1dcc7a785
dcugh <diana@gmail.com>
2019-09-16 10:13:58 +0200

    chore(client) update URL config for staging server

--- NCCRD_Client/app/js/config/serviceURLs.js
+++ NCCRD_Client/app/js/config/serviceURLs.js
@@ -10,33 +10,24 @@ let _mapServerBaseURL = ''
 
 
 if (CONSTANTS.DEV) {
-  // _apiBaseURL =  'http://app01.saeon.ac.za/nccrdtestapi/odata/' //'http://localhost:62553/odata/'
-  // _siteBaseURL = `${window.location.protocol}//${window.location.hostname}:${window.location.port}/`
-  // _ndmcBaseURL = 'http://app01.saeon.ac.za/ndmctestsite/'
-  // _ndaoBaseURL = 'http://app01.saeon.ac.za/ndaotestapi/odata/' //'https://localhost:44301/odata/'
-  // _ndaoSiteBaseURL = 'http://app01.saeon.ac.za/ndaotestsite/'
-  // _vmsBaseURL = 'http://app01.saeon.ac.za/vmstest/api/' //'http://localhost:64161/api/'
-  // _ssoBaseURL = 'https://identity.saeon.ac.za/' //'http://localhost:44320/'
-  // _mapServerBaseURL = 'https://ccis.environment.gov.za/map' //'http://app01.saeon.ac.za'
-  _apiBaseURL = 'https://ccis.environment.gov.za/nccrd/api/odata/';
-  _siteBaseURL = 'https://ccis.environment.gov.za/nccrd/';
-  _nccisBaseURL = 'https://ccis.environment.gov.za/#/'
-  _ndmcBaseURL = 'https://ccis.environment.gov.za/nhe/';
-  _ndaoBaseURL = 'https://ccis.environment.gov.za/ndao/api/odata/';
-  _ndaoSiteBaseURL = 'https://ccis.environment.gov.za/ndao/';
-  _vmsBaseURL = 'https://ccis.environment.gov.za/vms/api/';
+  _apiBaseURL = 'http://192.168.105.102/nccrd/api/odata';
+  _siteBaseURL = 'http://192.168.105.102/nccrd/';
+  _ndmcBaseURL = 'http://192.168.105.102/nhe';
+  _ndaoBaseURL = 'http://192.168.105.102/ndao/api/odata';
+  _ndaoSiteBaseURL = 'http://192.168.105.102/ndao/';
+  _vmsBaseURL = 'http://192.168.105.102/vms/api/';
   _ssoBaseURL = 'https://identity.saeon.ac.za/';
-  _mapServerBaseURL = 'https://ccis.environment.gov.za/map';
+  _mapServerBaseURL = 'http://192.168.105.102/map';
 }
 else if (CONSTANTS.TEST) {
-  _apiBaseURL = 'http://app01.saeon.ac.za/nccrdtestapi/odata/'
-  _siteBaseURL = 'http://app01.saeon.ac.za/nccrdtestsite/'
-  _ndmcBaseURL = 'http://app01.saeon.ac.za/ndmctestsite'
-  _ndaoBaseURL = 'http://app01.saeon.ac.za/ndaotestapi/odata/'
-  _ndaoSiteBaseURL = 'http://app01.saeon.ac.za/ndaotestsite/'
-  _vmsBaseURL = 'http://app01.saeon.ac.za/vmstest/api/'
-  _ssoBaseURL = 'http://identity.saeon.ac.za/'
-  _mapServerBaseURL = 'https://ccis.environment.gov.za/map' //'http://app01.saeon.ac.za'
+  _apiBaseURL = 'http://192.168.105.102/nccrd/api/odata';
+  _siteBaseURL = 'http://192.168.105.102/nccrd/';
+  _ndmcBaseURL = 'http://192.168.105.102/nhe';
+  _ndaoBaseURL = 'http://192.168.105.102/ndao/api/odata';
+  _ndaoSiteBaseURL = 'http://192.168.105.102/ndao/';
+  _vmsBaseURL = 'http://192.168.105.102/vms/api/';
+  _ssoBaseURL = 'https://identity.saeon.ac.za/';
+  _mapServerBaseURL = 'http://192.168.105.102/map'; //'http://app01.saeon.ac.za'
 }
 else if (CONSTANTS.PROD) {
   _apiBaseURL = 'https://ccis.environment.gov.za/nccrd/api/odata/';
